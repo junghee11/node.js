0a2c500ff38c1455ba7f712c6e774342b0a1175b
junghee11 <namjunghee11@naver.com>
2022-05-03 17:56:44 +0900

    async_non-blocking

---
+++ study11/createWriteStream.js
@@ -0,0 +1,14 @@
+"use strict"
+
+const fs = require('fs');
+
+const writeStream = fs.createWriteStream('./writeme2.txt');
+writeStream.on('finish', () => {
+    console.log('파일 쓰기 완료');
+});
+
+writeStream.write('이 글을 씁니다.\n');
+writeStream.write('한 번 더 씁니다.');
+writeStream.end();
+
+console.log('내용 추가 완료');
